684924025d0fb0c7b963a015b5dc3f51eff18864
jagra26 <31116911+jagra26@users.noreply.github.com>
2018-11-21 18:06:17 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,41 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    while(1)
+	    {
+	System.out.println("insira a operação");
+        Scanner entrada = new Scanner(System.in);
+	    Switch (operacao){
+            case 1:
+                //add
+                break;
+            case 2:
+                //remove
+                break;
+            case 3:
+                //Cartão de Ponto
+                break;
+            case 4:
+                //Resultado Venda
+                break;
+            case 5:
+                //taxa de serviço
+                break;
+            case 6:
+                //Alterar detalhes
+                break;
+            case 7:
+                //Rodar a folha
+                break;
+            case 8:
+                //Undo/redo
+                break;
+            case 9:
+                //Agenda de Pagamento
+                break;
+            case 10:
+                //Criação de Novas Agendas de Pagamento
+                break;
     }
+	    }
 }
