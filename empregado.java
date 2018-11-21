248390b47c95d449ad753a3e4d53ffb42c5ba203
jagra26 <31116911+jagra26@users.noreply.github.com>
2018-11-21 18:03:34 -0300

    Add files via upload

---
+++ empregado.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class empregado {
+    String nome;
+    String endereco;
+    double salario;
+    boolean sindicato;
+    int tipo;
+    int horas;
+    double taxa;
+    
+
+    public void setNome(string nome)
+    {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+}
